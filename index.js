61e1368344d855c341f91425d0aa369a539e9621
vyportfolio1 <136511458+vyportfolio1@users.noreply.github.com>
2023-12-12 00:23:45 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+     
+    const firstScreen = document.querySelector('.first');
+    const secondScreen = document.querySelector('.second');
+    const mainScreen = document.querySelector('.main-screen');
+
+    
+    setTimeout(() => {
+      firstScreen.style.display = 'none';
+      secondScreen.style.display = 'flex';
+      secondScreen.classList.remove('animate-out');
+      secondScreen.classList.add('animate-in');
+
+       
+      setTimeout(() => {
+        secondScreen.style.display = 'none';
+        mainScreen.style.display = 'flex';
+        mainScreen.classList.remove('animate-out');
+        mainScreen.classList.add('animate-in');
+      }, 5000);  
+    }, 6000);  
+  });
