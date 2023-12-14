adf769bea5041a68ba129caa79e27c135465752f
vyportfolio1 <136511458+vyportfolio1@users.noreply.github.com>
2023-12-15 01:04:33 +0200

    rock paper Scissors is done

--- ROCK PAPER SCISSORS/index.js
+++ ROCK PAPER SCISSORS/index.js
@@ -4,9 +4,12 @@ const rezult = document.querySelector(".rezult");
 const count = document.querySelector(".count");
 const reset = document.querySelector(".reset");
 
+const winIcon = document.querySelector(".fa-wine-bottle");
+const loseIcon = document.querySelector(".fa-heart-crack");
+const tiedIcon = document.querySelector(".fa-user-tie");
 
 let winCount = 0;
-let looseCount = 0;
+let loseCount = 0;
 let tiedCount = 0;
 
 buttons.forEach(button => {
@@ -19,35 +22,74 @@ buttons.forEach(button => {
         } else if (computerCh > 2/3 && computerCh < 1) {
             computerCh = "scissors";
         }
-        
-        const humanChoice = button.id; 
+
+        const humanChoice = button.id;
         game(humanChoice, computerCh);
-        rezult.innerHTML = `you choose: ${humanChoice}, computer": ${computerCh}`;s
+        showIcons(humanChoice, computerCh);
+        rezult.innerHTML = `You chose: ${humanChoice}, Computer chose: ${computerCh}`;
     });
 });
 
+function showIcons(human, computer) {
+    hideAllIcons();
+
+    document.querySelector(`.left-${human}`).classList.remove("hidden");
+    document.querySelector(`.right-${computer}`).classList.remove("hidden");
+}
+
+function hideAllIcons() {
+    const allIcons = document.querySelectorAll(".left-hand img, .right-hand img");
+    allIcons.forEach(icon => {
+        icon.classList.add("hidden");
+    });
+}
+
+function showWinIcon() {
+    winIcon.classList.remove("hidden");
+    loseIcon.classList.add("hidden");
+    tiedIcon.classList.add("hidden");
+}
+
+function showLoseIcon() {
+    winIcon.classList.add("hidden");
+    loseIcon.classList.remove("hidden");
+    tiedIcon.classList.add("hidden");
+}
+
+function showTiedIcon() {
+    winIcon.classList.add("hidden");
+    loseIcon.classList.add("hidden");
+    tiedIcon.classList.remove("hidden");
+}
 
 function game(human, computer) {
     if (human === computer) {
         event1.innerHTML = `It's a tie`;
         tiedCount++;
+        showTiedIcon();
     } else if ((human === "rock" && computer === "scissors") ||
                (human === "paper" && computer === "rock") ||
                (human === "scissors" && computer === "paper")) {
         event1.innerHTML = "You win";
         winCount++;
+        showWinIcon();
     } else {
         event1.innerHTML = "You lose";
-        looseCount++;
+        loseCount++;
+        showLoseIcon();
     }
-    count.innerHTML =`Win:${winCount} loose:${looseCount} tied:${tiedCount}`;
+    count.innerHTML = `Win: ${winCount} Lose: ${loseCount} Tied: ${tiedCount}`;
 }
 
 reset.addEventListener("click", () => {
     winCount = 0;
-    looseCount = 0;
+    loseCount = 0;
     tiedCount = 0;
-    count.innerHTML = `Win: ${winCount} Lose: ${looseCount} Tied: ${tiedCount}`;
+    count.innerHTML = `Win: ${winCount} Lose: ${loseCount} Tied: ${tiedCount}`;
     rezult.innerHTML = "";
     event1.innerHTML = "Events";
+    hideAllIcons();
+    winIcon.classList.add("hidden");
+    loseIcon.classList.add("hidden");
+    tiedIcon.classList.add("hidden");
 });
