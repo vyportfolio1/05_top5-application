c0c68c02e81b70943dc993775586d339e6e37839
vyportfolio1 <136511458+vyportfolio1@users.noreply.github.com>
2023-12-20 00:01:46 +0200

    fix memory

--- MEMORY GAME/index.js
+++ MEMORY GAME/index.js
@@ -1,1 +1,80 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const cards = document.querySelectorAll(".card");
+    const restartBtn = document.getElementById("restartBtn");
 
+    let isFlipped = false;
+    let firstCard, secondCard;
+    let lockBoard = false;
+
+ 
+    const shuffledCards = Array.from(cards).sort(() => Math.random() - 0.5);
+    document.querySelector(".memory-game").innerHTML = "";
+    shuffledCards.forEach(card => {
+        document.querySelector(".memory-game").appendChild(card);
+    });
+
+    restartBtn.addEventListener("click", restartGame);
+
+    cards.forEach(card => card.addEventListener("click", flipCard));
+
+    function flipCard() {
+        if (lockBoard) return;
+        if (this === firstCard) return;
+
+        this.classList.add("flip");
+
+        if (!isFlipped) {
+      
+            isFlipped = true;
+            firstCard = this;
+            return;
+        }
+
+     
+        isFlipped = false;
+        secondCard = this;
+
+        checkForMatch();
+    }
+
+    function checkForMatch() {
+        const isMatch = firstCard.dataset.card === secondCard.dataset.card;
+
+        isMatch ? disableCards() : unflipCards();
+    }
+
+    function disableCards() {
+        firstCard.removeEventListener("click", flipCard);
+        secondCard.removeEventListener("click", flipCard);
+
+        removeCardsFromBoard(firstCard, secondCard);
+
+        resetBoard();
+    }
+
+    function unflipCards() {
+        lockBoard = true;
+
+        setTimeout(() => {
+            firstCard.classList.remove("flip");
+            secondCard.classList.remove("flip");
+
+            resetBoard();
+        }, 1000);
+    }
+
+    function resetBoard() {
+        [isFlipped, lockBoard] = [false, false];
+        [firstCard, secondCard] = [null, null];
+    }
+
+    function restartGame() {
+        location.reload();
+    }
+
+    function removeCardsFromBoard(card1, card2) {
+
+        card1.style.display = "none";
+        card2.style.display = "none";
+    }
+});
